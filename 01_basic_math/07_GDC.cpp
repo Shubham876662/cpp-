048de2a1041cf316a9e54eeeb5bec811fe9de218
Shubham876662 <shubhamgagare829@gmail.com>
2026-04-08 06:19:26 +0000

    day_04.2

--- 01_basic_math/07_GDC.cpp
+++ 01_basic_math/07_GDC.cpp
@@ -1,24 +1,33 @@
 # include <bits/stdc++.h>
 using namespace std;
 
-int divisr(int m,int n){
+int god_bf(int m,int n){
     int x = 0;
     for(int i=1;i<=min(m,n);i++){
         if(n%i == 0 && m%i == 0){
             x=i;
         }
     }
-    cout<<"The Greatest divisor factor is : "<<x<<endl;
-    return 0;
+    return x;
+}
+
+int god_optimal(int m,int n){
+    while(n != 0){
+        int remind = m%n;
+        m = n;
+        n = remind;
+    }
+    return m;
 }
 
 int main(){
-    int m ;
-    int n ;
+    int m = 18;
+    int n = 12;
+    
     if(cin>>m && cin>>n){
-        divisr(m,n);
+        cout<<"Bruteforce method"<<endl;
+        cout<<"the GOD is : "<<god_bf(m,n)<<endl;
+        cout<<"optimal method"<<endl;
+        cout<<"the GOD is : "<<god_optimal(m,n)<<endl;
     }
-    
-
-    
 }
